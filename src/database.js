357f5461b6d0902bfc7d83de9b5a241a59f24c6d
brunoRobs <git.brunoRobs.excal@gmail.com>
2024-07-30 00:49:29 -0300

    Implementação via JS operante

--- src/database.js
+++ src/database.js
@@ -11,54 +11,90 @@ const pool = new Pool({
 async function search(title) {
     const client = await pool.connect();
     if (title) {
-        const query = 'SELECT * FROM Livro WHERE titulo ILIKE $1'
+        title = `%${title}%`;
+        const query = 'SELECT * FROM Book WHERE title ILIKE $1 ORDER BY title ASC'
         const values = [title]
         const result = await client.query(query, values);
         const books = result.rows;
         return books;
     }
-    const result = await client.query('SELECT * FROM Livro');
+    const result = await client.query('SELECT * FROM Book ORDER BY title ASC');
     const books = result.rows;
     client.release();
     return books;
 }
 
-async function register(userData, credentials, address) {
+async function filterByGenre(genre) {
     const client = await pool.connect();
 
-    let query = "SELECT cpf FROM DonboUser WHERE cpf = $1";
-    let values = [userData.cpf];
+    let books;
 
-    const exists = await client.query(query, values).then(response => response.rowCount);
+    if (genre) {
+        const query = 'SELECT * FROM Literature WHERE genre = $1';
+        const value = [genre]
 
-    if (exists) return false;
-    else {
-        try {
-            query = "INSERT INTO DonboUser(cpf, firstName, lastName, birthDate) VALUES ($1, $2, $3, $4)";
-            values = [userData.cpf, userData.firstName, userData.lastName, userData.birthDate];
-            let success = (await client.query(query, values)).rowCount;
+        const result = await client.query(query, value);
+        books = result.rows;
+    } else {
+        const query = 'SELECT * FROM Literature';
 
-            query = "INSERT INTO DonboUserCredentials(cpf, email, username, userpassword) VALUES ($1, $2, $3, $4)";
-            values = [userData.cpf, credentials.email, credentials.username, credentials.password];
-            success = (await client.query(query, values)).rowCount;
-
-            query = "INSERT INTO DonboUserAddress(cpf, country, countrystate, city, neighborhood, street, streetnumber, complement, zipcode) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);";
-            values = [userData.cpf, address.country, address.countryState, address.city, address.neighborhood, address.street, address.streetNumber, address.complement, address.zipCode];
-            success = (await client.query(query, values)).rowCount;
-
-            return success;
-        } catch (error) {
-            return false;
-        } finally {
-            client.release();
-        }
+        const result = await client.query(query);
+        books = result.rows;
     }
+
+    client.release();
+    return books;
+}
+
+async function filterBySubject(subject) {
+    const client = await pool.connect();
+
+    let books;
+    if (subject) {
+        const query = 'SELECT * FROM Academic WHERE subject = $1';
+        const value = [subject]
+
+        const result = await client.query(query, value);
+        books = result.rows;
+    } else {
+        const query = 'SELECT * FROM Academic';
+
+        const result = await client.query(query);
+        books = result.rows;
+    }
+
+    client.release();
+    return books;
+}
+
+async function genreList() {
+    const client = await pool.connect();
+
+    const query = 'SELECT genre FROM Literature GROUP BY genre ORDER BY genre ASC';
+
+    const result = await client.query(query);
+    const books = result.rows;
+
+    client.release();
+    return books;
+}
+
+async function subjectList() {
+    const client = await pool.connect();
+
+    const query = 'SELECT subject FROM Academic GROUP BY subject ORDER BY subject ASC';
+
+    const result = await client.query(query);
+    const books = result.rows;
+
+    client.release();
+    return books;
 }
 
 async function login(username, password) {
     const client = await pool.connect();
 
-    let query = "SELECT * FROM DonboUserCredentials WHERE username = $1 AND userPassword = $2";
+    let query = "SELECT * FROM Credentials WHERE username = $1 AND userPassword = $2";
     let values = [username, password];
 
     const exists = await client.query(query, values).then(response => response.rowCount);
@@ -67,19 +103,45 @@ async function login(username, password) {
     return false;
 }
 
-async function filter(genero) {
+async function register(userData, credentials, address) {
     const client = await pool.connect();
 
-    const result = await client.query(`SELECT titulo FROM Livro WHERE genero ILIKE '${genero}'`);
-    const books = result.rows;
+    let query = "SELECT cpf FROM UserDonbo WHERE cpf = $1";
+    let values = [userData.cpf];
 
-    client.release();
-    return books;
+    const exists = await client.query(query, values).then(response => response.rowCount);
+
+    if (exists) return false;
+    else {
+        try {
+            query = "INSERT INTO UserDonbo(cpf, firstName, lastName, birthDate) VALUES ($1, $2, $3, $4)";
+            values = [userData.cpf, userData.firstName, userData.lastName, userData.birthDate];
+            let success = (await client.query(query, values)).rowCount;
+
+            query = "INSERT INTO Credentials(cpf, email, username, userpassword) VALUES ($1, $2, $3, $4)";
+            values = [userData.cpf, credentials.email, credentials.username, credentials.password];
+            success = (await client.query(query, values)).rowCount;
+
+            query = "INSERT INTO Address(cpf, country, countrystate, city, neighborhood, street, streetnumber, complement, zipcode) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);";
+            values = [userData.cpf, address.country, address.countryState, address.city, address.neighborhood, address.street, address.streetNumber, address.complement, address.zipCode];
+            success = (await client.query(query, values)).rowCount;
+
+            return success;
+        } catch (error) {
+            console.log(error);
+            return false;
+        } finally {
+            client.release();
+        }
+    }
 }
 
 module.exports = {
     search,
     register,
     login,
-    filter
+    filterByGenre,
+    filterBySubject,
+    genreList,
+    subjectList
 }
--- src/index.js
+++ src/index.js
@@ -20,10 +20,11 @@ app.use(express.json());
 
 app.use(express.static('../public'));
 
-app.get('/search', async (req, res) => {
-    const title = req.query.title;
-    const books = await database.search(title)
-    res.send(books);
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const success = await database.login(username, password);
+    if (success) res.status(200).send();
+    else res.status(400).send();
 });
 
 app.post('/register', async (req, res) => {
@@ -33,190 +34,48 @@ app.post('/register', async (req, res) => {
     else res.status(400).send();
 });
 
-app.post('/login', async (req, res) => {
-    const { username, password } = req.body;
-    const success = await database.login(username, password);
-    if (success) res.status(200).send();
-    else res.status(400).send();
+app.get('/search', async (req, res) => {
+    const title = req.query.title;
+    const books = await database.search(title)
+    res.send(books);
 });
 
 app.get('/filter', async (req, res) => {
-    const genero = req.query.genero;
-    const success = await database.filter(genero);
-    if (success) res.status(200).send(success);
-    else res.status(400).send();
+    try {
+        if (req.query.type === 'literature') {
+            const genre = req.query.genre;
+            const success = await database.filterByGenre(genre);
+            if (success) res.status(200).send(success);
+            else res.status(400).send();
+        }
+        else if (req.query.type === 'academic') {
+            const subject = req.query.subject;
+            const success = await database.filterBySubject(subject);
+            if (success) res.status(200).send(success);
+            else res.status(400).send();
+        }
+    } catch (error) {
+        console.error(error);
+    }
 });
 
-const server = app.listen(8080, '26.60.40.255', () => {
+app.get('/list', async (req, res) => {
+    try {
+        if (req.query.type === 'genre') {
+            const genreList = await database.genreList();
+            if (genreList.length) res.status(200).send(genreList);
+            else res.status(400).send();
+        }
+        else if (req.query.type === 'subject') {
+            const subjectList = await database.subjectList();
+            if (subjectList.length) res.status(200).send(subjectList);
+            else res.status(400).send();
+        }
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+const server = app.listen(8080, 'localhost', () => {
     console.log('Server inicialized...');
 });
-
-// const fs = require('fs');
-
-// async function inserirImagem(caminhoDaImagem ,isbn) {
-//     try {
-//         const client = await pool.connect();
-//         const dadosDaImagem = fs.readFileSync(caminhoDaImagem);
-
-//         const query = 'UPDATE Livro SET imagem = $1 WHERE isbn = $2';
-//         const values = [dadosDaImagem, isbn];
-
-//         console.log(dadosDaImagem);
-
-//         await client.query(query, values);
-//         client.release();
-//         console.log('Imagem inserida com sucesso.');
-//     } catch (err) {
-//         console.error('Erro ao inserir imagem:', err);
-//     }
-// }
-
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//     user: 'postgres',
-//     host: 'localhost',
-//     database: 'donbo_db',
-//     password: '123456',
-//     port: 5432
-// });
-
-// async function inserirImagem() {
-//     try {
-//         const client = await pool.connect();
-
-//         const livros =  [
-//             {
-//                 titulo: "Dom Casmurro",
-//                 autor: "Machado de Assis",
-//                 editora: "Editora 34",
-//                 genero: "Romance"
-//             },
-//             {
-//                 titulo: "Grande Sertão: Veredas",
-//                 autor: "João Guimarães Rosa",
-//                 editora: "Nova Fronteira",
-//                 genero: "Romance"
-//             },
-//             {
-//                 titulo: "O Morro dos Ventos Uivantes",
-//                 autor: "Emily Brontë",
-//                 editora: "Martin Claret",
-//                 genero: "Romance"
-//             },
-//             {
-//                 titulo: "1984",
-//                 autor: "George Orwell",
-//                 editora: "Companhia das Letras",
-//                 genero: "Ficção Científica"
-//             },
-//             {
-//                 titulo: "Cem Anos de Solidão",
-//                 autor: "Gabriel García Márquez",
-//                 editora: "Record",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "O Senhor dos Anéis",
-//                 autor: "J.R.R. Tolkien",
-//                 editora: "HarperCollins Brasil",
-//                 genero: "Fantasia"
-//             },
-//             {
-//                 titulo: "A Revolução dos Bichos",
-//                 autor: "George Orwell",
-//                 editora: "Companhia das Letras",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "Orgulho e Preconceito",
-//                 autor: "Jane Austen",
-//                 editora: "Martin Claret",
-//                 genero: "Romance"
-//             },
-//             {
-//                 titulo: "O Apanhador no Campo de Centeio",
-//                 autor: "J.D. Salinger",
-//                 editora: "Editora do Autor",
-//                 genero: "Romance"
-//             },
-//             {
-//                 titulo: "A Metamorfose",
-//                 autor: "Franz Kafka",
-//                 editora: "Companhia das Letras",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "A Menina que Roubava Livros",
-//                 autor: "Markus Zusak",
-//                 editora: "Intrínseca",
-//                 genero: "Drama"
-//             },
-//             {
-//                 titulo: "O Pequeno Príncipe",
-//                 autor: "Antoine de Saint-Exupéry",
-//                 editora: "Agir",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "A Guerra dos Tronos",
-//                 autor: "George R.R. Martin",
-//                 editora: "LeYa",
-//                 genero: "Fantasia"
-//             },
-//             {
-//                 titulo: "O Código Da Vinci",
-//                 autor: "Dan Brown",
-//                 editora: "Arqueiro",
-//                 genero: "Suspense"
-//             },
-//             {
-//                 titulo: "O Alquimista",
-//                 autor: "Paulo Coelho",
-//                 editora: "Rocco",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "Harry Potter e a Pedra Filosofal",
-//                 autor: "J.K. Rowling",
-//                 editora: "Rocco",
-//                 genero: "Fantasia"
-//             },
-//             {
-//                 titulo: "Moby Dick",
-//                 autor: "Herman Melville",
-//                 editora: "Cosac Naify",
-//                 genero: "Aventura"
-//             },
-//             {
-//                 titulo: "Crime e Castigo",
-//                 autor: "Fiódor Dostoiévski",
-//                 editora: "Editora 34",
-//                 genero: "Ficção"
-//             },
-//             {
-//                 titulo: "O Iluminado",
-//                 autor: "Stephen King",
-//                 editora: "Suma",
-//                 genero: "Terror"
-//             },
-//             {
-//                 titulo: "A Divina Comédia",
-//                 autor: "Dante Alighieri",
-//                 editora: "Editora 34",
-//                 genero: "Poesia"
-//             }
-//         ];
-
-//         for (let i = 0; i < livros.length; i++) {
-//             const query = `INSERT INTO Livro (titulo, autor, editora, genero) VALUES ('${livros[i].titulo}', '${livros[i].autor}', '${livros[i].editora}', '${livros[i].genero}')`;
-//             await client.query(query);
-//         }
-
-//         client.release();
-//     } catch (err) {
-//         console.error('Erro ao inserir imagem:', err);
-//     }
-// }
-
-// inserirImagem()
